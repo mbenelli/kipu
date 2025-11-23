5fb4c6c3a71aef98d82cb3c4b07967b77f9e44a6
Marco Benelli <mbenelli@fastmail.com>
2025-11-23 22:16:00 +0100

    Add rest client

--- lib/Kipu/GitHub/Client.hs
+++ lib/Kipu/GitHub/Client.hs
@@ -16,6 +16,7 @@ import           Data.Aeson
 import           Data.ByteString.Char8 (pack)
 import           GHC.Generics          (Generic)
 import           Kipu.Config
+import           Kipu.GitHub.Types     (Repository)
 import           Network.HTTP.Req
 
 newtype Query = Query
@@ -43,8 +44,38 @@ typeInfo t = Query {query = "query { __type(name: \"" <> t <> "\") { name kind d
 organization :: Query
 organization = typeInfo "Organization"
 
+orgInfo :: Text -> Query
+orgInfo n = Query {
+  query =
+    "query { organization(login: \"" <> n <> "\") "
+    <> "{ archivedAt createdAt description email id login"
+    <> " membersWithRole(first: 10) {totalCount edges { node { name }}}"
+    <> " name"
+    <> " packages(first: 10) {totalCount}"
+--    <> " projectV2(first: 10) { edges {node { name }}}"
+--    <> " projectsUrl(first: 10) {totalCount}"
+    <> " repositories(first: 100) { totalCount nodes { name } }"
+--    <> " team(first: 10) {}"
+    <> " teamsUrl"
+    <> " url"
+    <> "}}"
+}
+
+repositoryConnection :: Query
+repositoryConnection = typeInfo "RepositoryConnection"
+
 repository :: Query
 repository = typeInfo "Repository"
+
+mbrepos :: Query
+mbrepos = Query {
+  query = "query { search(query:\"owner:mercedes-benz\" type: REPOSITORY first: 5 after: null) {repositoryCount}}"
+}
+
+mbrepo :: Query
+mbrepo = Query {
+  query = "query { search(query:\"name:DnA forks:>20\" type: REPOSITORY first: 5 after: null) {repositoryCount nodes { ... on Repository {name }}}}"
+}
 
 user :: Query
 user = typeInfo "User"
@@ -64,10 +95,35 @@ graphql q t = do
       request = req POST ep body bsResponse prms
   responseBody <$> runReq defaultHttpConfig request
 
-run :: Query -> IO ByteString
-run q = do
+runGraphql :: Query -> IO ByteString
+runGraphql q = do
   file <- defaultConfigFile
   c <- readConfig file
   case c of
     Left e    -> return $ pack $ show e
     Right cfg -> graphql q (encodeUtf8 $ githubToken cfg)
+
+type Endpoint = Text
+
+repos :: Text -> Token -> IO [Repository]
+repos org ghToken = do
+  let ep = https "api.github.com" /: "orgs" /: org /: "repos"
+      prms =
+        mconcat
+          [ header "Authorization" ("bearer " <> ghToken),
+            header "User-Agent" "agent",
+            header "Accept" "application/vnd.github+json",
+            header "X-GitHub-Api-Version" "2022-11-28"
+          ]
+      body = NoReqBody
+      request = req GET ep body jsonResponse prms
+  responseBody <$> runReq defaultHttpConfig request
+
+run :: Text -> IO [Repository]
+run org = do
+  file <- defaultConfigFile
+  c <- readConfig file
+  case c of
+    Left e    -> pure [] -- return $ pack $ show e
+    Right cfg -> repos org (encodeUtf8 $ githubToken cfg)
+
--- lib/Kipu/GitHub/Types.hs
+++ lib/Kipu/GitHub/Types.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 -- |
@@ -8,15 +7,91 @@
 -- Copyright: (c) Marco Benelli 2025
 -- License: ISC
 -- Maintainer: mbenelli@fastmail.com
+--
 module Kipu.GitHub.Types where
 
-import BasicPrelude
-import Data.Aeson
-import Data.Aeson.Types (Parser)
-import Data.Text (Text)
-import GHC.Generics (Generic)
+import           BasicPrelude
+import           Data.Aeson
+import           GHC.Generics     (Generic)
+import           Kipu.JsonOptions (options)
 
-data Foo = Foo
-  {
-  }
-  deriving (Show, Generic)
+data User = User
+  { user_login               :: Text
+--  , user_id                  :: Int
+--  , user_node_id             :: Text
+  , user_avatar_url          :: Text -- Url
+--  , user_gravatar_id         :: Text
+  , user_url                 :: Text -- Url
+  , user_html_url            :: Text -- Url
+  , user_followers_url       :: Text -- Url
+  , user_following_url       :: Text -- Url
+  , user_gists_url           :: Text -- Url
+  , user_starred_url         :: Text -- Url
+  , user_subscriptions_url   :: Text -- Url
+  , user_organizations_url   :: Text -- Url
+  , user_repos_url           :: Text -- Url
+  , user_events_url          :: Text -- Url
+  , user_received_events_url :: Text -- Url
+  , user_type                :: Text -- UserType
+  , user_site_admin          :: Bool
+  } deriving (Show, Generic)
+
+instance FromJSON User where
+  parseJSON = genericParseJSON options
+
+instance ToJSON User where
+  toJSON = genericToJSON options
+
+data Permissions = Permissions
+  { permissions_admin    :: Maybe Bool
+  , permissions_maintain :: Maybe Bool
+  , permissions_push     :: Maybe Bool
+  , premission_triage    :: Maybe Bool
+  , permissions_pull     :: Maybe Bool
+  } deriving (Show, Generic)
+
+instance FromJSON Permissions where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Permissions where
+  toJSON = genericToJSON options
+
+data Repository = Repository
+  { repository_id                :: Int
+  , repository_name              :: Text
+  , repository_full_name         :: Text
+--  , repository_owner             :: User
+  , repository_private           :: Bool
+  , repository_html_url          :: Text -- Url
+  , repository_description       :: Maybe Text
+  , repository_fork              :: Bool
+  , repository_url               :: Text -- Url
+  , repository_language          :: Maybe Text
+  , repository_forks_count       :: Int
+  , repository_stargazer_count   :: Maybe Int
+  , repository_watchers_count    :: Int
+  , repository_size              :: Int
+  , repository_default_branch    :: Text
+  , repository_open_issues_count :: Int
+  , repository_is_template       :: Bool
+  , repository_topics            :: Maybe [Text]
+  , repository_has_issues        :: Bool
+  , repository_has_projects      :: Bool
+  , repository_has_wiki          :: Bool
+  , repository_has_pages         :: Bool
+  , repository_has_downloads     :: Bool
+  , repository_has_discussions   :: Bool
+  , repository_archived          :: Bool
+  , repository_disabled          :: Bool
+  , repository_visibility        :: Text -- Visibility
+  , repository_pushed_at         :: Maybe Text -- Datetime
+  , repository_created_at        :: Maybe Text -- Datetime
+  , repository_updated_at        :: Maybe Text -- Datetime
+  , repository_permissions       :: Permissions
+  } deriving (Show, Generic)
+
+instance FromJSON Repository where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Repository where
+  toJSON = genericToJSON options
