ac8b96f6eaa2856d38c11a7624d92c7b146c4d6a
Marco Benelli <mbenelli@fastmail.com>
2025-12-09 19:36:56 +0100

    github: add types for PRs

--- lib/Kipu/GitHub/GraphQL/Types.hs
+++ lib/Kipu/GitHub/GraphQL/Types.hs
@@ -24,6 +24,9 @@ data PageInfo = PageInfo
   , hasPreviousPage :: Bool
   } deriving (Show, Generic, FromJSON, ToJSON)
 
+class Paged p where
+  pageInfo :: p -> PageInfo
+
 data Repository = Repository
   { archivedAt     :: Maybe UTCTime
   , createdAt      :: UTCTime
@@ -42,9 +45,18 @@ data Repository = Repository
   } deriving (Show, Generic, FromJSON, ToJSON)
 
 data Repositories = Repositories
-  { nodes    :: [Repository]
-  , pageInfo :: PageInfo
-  } deriving (Show, Generic, FromJSON, ToJSON)
+  { repositories_nodes    :: [Repository]
+  , repositories_pageInfo :: PageInfo
+  } deriving (Show, Generic)
+
+instance FromJSON Repositories where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Repositories where
+  toJSON = genericToJSON options
+
+instance Paged Repositories where
+  pageInfo = repositories_pageInfo
 
 newtype Organization = Organization
   { repositories :: Repositories
@@ -64,9 +76,133 @@ instance FromJSON RepoResult where
 instance ToJSON RepoResult where
   toJSON = genericToJSON options
 
-
 getRepos :: RepoResult -> [Repository]
-getRepos = nodes . repositories . organization . repo_data
+getRepos = repositories_nodes . repositories . organization . repo_data
 
 getPageInfo:: RepoResult -> PageInfo
 getPageInfo = pageInfo . repositories . organization . repo_data
+
+-- | Git Author
+--
+-- The date comes from  an ISO-8601 encoded date string
+-- not converted in UTC. (according to GitHub documentation)
+--
+data GitAuthor = GitAuthor
+  { gitAuthor_date  :: Maybe UTCTime
+  , gitAuthor_email :: Maybe Text
+  , gitAuthor_name  :: Maybe Text
+  , gitAuthor_user  :: Maybe Actor
+  } deriving (Show, Generic)
+
+instance FromJSON GitAuthor where
+  parseJSON = genericParseJSON options
+
+instance ToJSON GitAuthor where
+  toJSON = genericToJSON options
+
+data GitAuthors = GitAuthors
+  { gitAuthors_nodes      :: [GitAuthor]
+  , gitAuthors_pageInfo   :: PageInfo
+  , gitAuthors_totalCount :: Int
+  } deriving (Show, Generic)
+
+instance FromJSON GitAuthors where
+  parseJSON = genericParseJSON options
+
+instance ToJSON GitAuthors where
+  toJSON = genericToJSON options
+
+data Commit = Commit
+  { commit_authors        :: GitAuthors
+  , commit_abbreviatedOid :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON Commit where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Commit where
+  toJSON = genericToJSON options
+
+newtype PullRequestCommit = PullRequestCommit
+  { pullRequestCommit_commit :: Commit
+  } deriving (Show, Generic)
+
+instance FromJSON PullRequestCommit where
+  parseJSON = genericParseJSON options
+
+instance ToJSON PullRequestCommit where
+  toJSON = genericToJSON options
+
+data Commits = Commits
+  { commits_nodes      :: [PullRequestCommit]
+  , commits_pageInfo   :: PageInfo
+  , commits_totalCount :: Int
+  } deriving (Show, Generic)
+
+instance FromJSON Commits where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Commits where
+  toJSON = genericToJSON options
+
+newtype Actor = Actor
+  { actor_login :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON Actor where
+  parseJSON = genericParseJSON options
+
+instance ToJSON Actor where
+  toJSON = genericToJSON options
+
+data PullRequest = PullRequest
+  { pullRequest_author      :: Maybe Actor
+  , pullRequest_closed      :: Bool
+  , pullRequest_closedAt    :: Maybe UTCTime
+  , pullRequest_commits     :: Commits
+  , pullRequest_createdAt   :: UTCTime
+  , pullRequest_id          :: Text
+  , pullRequest_merged      :: Bool
+  , pullRequest_mergedAt    :: Maybe UTCTime
+  , pullRequest_number      :: Int
+  , pullRequest_permalink   :: Text
+  , pullRequest_publishedAt :: Maybe UTCTime
+  , pullRequest_title       :: Text
+  , pullRequest_updatedAt   :: UTCTime
+  , pullRequest_url         :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON PullRequest where
+  parseJSON = genericParseJSON options
+
+instance ToJSON PullRequest where
+  toJSON = genericToJSON options
+
+data PullRequests = PullRequests
+  { pullRequests_nodes    :: [PullRequest]
+  , pullRequests_pageInfo :: PageInfo
+  } deriving (Show, Generic)
+
+instance FromJSON PullRequests where
+  parseJSON = genericParseJSON options
+
+instance ToJSON PullRequests where
+  toJSON = genericToJSON options
+
+newtype PrRepo = PrRepo
+  { pullRequests :: PullRequests
+  } deriving (Show, Generic, FromJSON, ToJSON)
+
+newtype PrData = PrData
+  { repository:: PrRepo
+  } deriving (Show, Generic, FromJSON, ToJSON)
+
+newtype PrResult = PrResult
+  { pr_data :: PrData
+  } deriving (Show, Generic)
+
+instance FromJSON PrResult where
+  parseJSON = genericParseJSON options
+
+instance ToJSON PrResult where
+  toJSON = genericToJSON options
